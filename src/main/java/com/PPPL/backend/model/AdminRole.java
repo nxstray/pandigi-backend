ba1fbc19569bceea7ecc2223eedaf89c62906413
nxstray <afwanapriansyah@gmail.com>
2025-11-29 21:19:36 +0700

    done: added new enum class for admin roles

---
+++ src/main/java/com/PPPL/backend/model/AdminRole.java
@@ -0,0 +1,7 @@
+package com.PPPL.backend.model;
+
+public enum AdminRole {
+    SUPER_ADMIN,  // Full access
+    ADMIN,        // Standard admin access
+    MANAGER       // hanya lihat, tidak bisa delete
+}
