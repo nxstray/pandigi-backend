c27de252546d12f6730a195aacd1b0fbde035cab
nxstray <afwanapriansyah@gmail.com>
2026-01-05 23:39:36 +0700

    feat: new dto for register new manager

---
+++ src/main/java/com/PPPL/backend/data/RegisterManagerRequest.java
@@ -0,0 +1,13 @@
+package com.PPPL.backend.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterManagerRequest {
+    private String namaLengkap;
+    private String email;
+}
