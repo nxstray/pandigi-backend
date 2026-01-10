bfa05fae4bd61608a9673c7c8e7024f59f67162f
nxstray <afwanapriansyah@gmail.com>
2026-01-10 22:51:26 +0700

    feat: new dtos for forgot and reset password

---
+++ src/main/java/com/PPPL/backend/data/ResetPasswordRequest.java
@@ -0,0 +1,13 @@
+package com.PPPL.backend.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetPasswordRequest {
+    private String token;
+    private String newPassword;
+}
