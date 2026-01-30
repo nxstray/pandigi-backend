f2472cbbf027bb18281b399e422f55e2257328cc
nxstray <afwanapriansyah@gmail.com>
2026-01-31 05:01:58 +0700

    delete (restructured): file structure adjustment for better management / maintenance

--- src/main/java/com/PPPL/backend/repository/UploadFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.PPPL.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.PPPL.backend.model.file.UploadFile;
-
-@Repository
-public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {}
